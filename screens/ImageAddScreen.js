1c81dbe84c29d843c0ebbcca4d242381998fd3bb
milk717 <robolindasoo@gmail.com>
2023-04-27 13:43:40 +0900

    feat: 이미지 등록 ui 생성

--- screens/ImageAddScreen.js
+++ screens/ImageAddScreen.js
@@ -1,10 +1,52 @@
-import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {StyleSheet, View, Image} from 'react-native';
+import {Text, Card, Button} from 'react-native-paper';
+import theme from '../resources/style/theme';
+import {useState} from 'react';
+import logger from 'redux-logger';
 
 export default function ImageAddScreen() {
+  const initImageUrl =
+    'https://cdn-icons-png.flaticon.com/512/3566/3566345.png';
+  const [image, setImage] = useState(initImageUrl);
   return (
-    <View>
-      <Text>이미지 추가 등록 화면</Text>
+    <View style={styles.container}>
+      <Card style={styles.cardContainer} onPress={() => console.log('누름')}>
+        <Image
+          style={
+            image === initImageUrl ? styles.defaultCardCover : styles.cardCover
+          }
+          source={{uri: image}}
+        />
+      </Card>
+      <View>
+        <Button
+          icon="content-save"
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+          사진 저장하기
+        </Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.elevation.level0,
+    padding: 16,
+  },
+  cardContainer: {
+    height: 600,
+    marginBottom: 16,
+  },
+  cardCover: {
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  defaultCardCover: {
+    height: '100%',
+    resizeMode: 'contain',
+    marginHorizontal: 40,
+  },
+});
